11c71172b091b00585cc6e32e46006db45f8f255
Lucas Bonomi <lucas.bonomi@gmail.com>
2016-06-26 14:33:01 +0200

    Better style n'stuff

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -23,3 +23,31 @@ document.getElementById('input').addEventListener('input', function (e, x) {
         imgHolder.appendChild(img);
     };
 });
+
+// bullgit badge.js v2
+(function (size,image,url,alt) {
+  var a = document.createElement('a');
+  var img = document.createElement('img');
+  a.href = url;
+  img.src = image;
+  img.alt = alt;
+
+  a.style.position = 'fixed';
+  a.style.bottom = 0;
+  a.style.left = 0;
+  a.style.backgroundColor = "white";
+  a.style.height =  size;
+  a.style.width = size;
+  a.style.WebkitClipPath = "polygon(0 0, 0% 100%, 100% 100%)";
+  a.style.clipPath = "polygon(0 0, 0% 100%, 100% 100%)";
+
+  img.style.width = '60%';
+  img.style.height = '60%';
+  img.style.position = 'absolute';
+  img.style.bottom = 0;
+  img.style.left = 0;
+
+  a.appendChild(img);
+  document.documentElement.appendChild(a);
+
+})("180px","https://bullg.it/media/vectors/bullgit-logo.svg","https://bullg.it","bullgit logo");
